cec159c9e1587e0ceea3e3a2eb22b1267ea580e2
Bianca Maria Soares <124786406+biancaasoares@users.noreply.github.com>
2024-10-17 17:15:13 -0300

    home

--- app/src/main/java/br/senai/sp/itermob/screens/TelaHome.kt
+++ app/src/main/java/br/senai/sp/itermob/screens/TelaHome.kt
@@ -1,21 +1,48 @@
 package br.senai.sp.itermob.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import br.senai.sp.itermob.R
+import br.senai.sp.itermob.ui.theme.ItermobTheme
 
 @Composable
 fun TelaHome(navController: NavController) {
     Surface (
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Gray)
     ) {
-        Column {  }
+        Box(
+            modifier = Modifier
+                .background(color = Color.Yellow)
+               
+        ){
+
+        }
     }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun TelaHomePrev() {
+    ItermobTheme{
+        TelaHome(navController = NavHostController(LocalContext.current))
+    }
+
 }
