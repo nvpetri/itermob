b359da3b24bb811f437cb4f6023055fc9c973a2d
Bianca Maria Soares <124786406+biancaasoares@users.noreply.github.com>
2024-11-12 17:14:39 -0300

    commit geral 12.11

--- app/src/main/java/br/senai/sp/itermob/MainActivity.kt
+++ app/src/main/java/br/senai/sp/itermob/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : ComponentActivity() {
 
                 NavHost(
                     navController = navigationControl,
-                    startDestination = "dadosPessoais"
+                    startDestination = "splash"
                 )
                 {
                     composable(route = "splash") { TelaSplash(navigationControl) }
